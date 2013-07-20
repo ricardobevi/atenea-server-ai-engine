7f66f31d0207d7b507bf0acd5508a94c8618312a
lea <lea@localhost>
2013-07-20 12:16:18 -0300

    Agrego al algoritmo VerySimple un switch que devuelve los comandos correspondientes cuando recibe una orden.

--- src/main/java/org/squadra/atenea/aiengine/algorithm/VerySimpleResponseAlgorithm.java
+++ src/main/java/org/squadra/atenea/aiengine/algorithm/VerySimpleResponseAlgorithm.java
@@ -9,19 +9,39 @@ public class VerySimpleResponseAlgorithm  implements AbstractAlgorithm{
 
 	public Message execute(Message inputMessage) {
 		
-		String inputText = inputMessage.getText();
+		String inputText = inputMessage.getText().toLowerCase();
 		
 		Message response = new Message();
 		
 		BasicQuery query = new BasicQuery();
 		
-		ArrayList<String> responses = query.getRelatedWords( inputText.toLowerCase() );
+		ArrayList<String> responses = query.getRelatedWords( inputText );
 		
 		if ( responses != null && 
 			 !responses.isEmpty() && 
 			 !responses.get(0).isEmpty() ){
 			
-			response = new Message(responses.get(0), Message.ASSERTION);
+			if (inputText.contains("abrir") || inputText.contains("cerrar")) {
+				response.setType(Message.ORDER);
+				switch (inputText) {
+					case "abrir bloc de notas":
+						response.setOrder("notepad.exe");
+						break;
+					case "cerrar bloc de notas":
+						response.setOrder("taskkill /IM notepad.exe");
+						break;
+					case "abrir panel de control":
+						response.setOrder("control");
+						break;
+					case "abrir administrador de tareas":
+						response.setOrder("taskmgr");
+						break;
+				}
+			}
+			else {
+				response.setType(Message.ASSERTION);
+			}
+			response.setText(responses.get(0));
 			
 		} else {
 			response = new Message("Disculpa, no logre entenderte, por favor repitemelo.", Message.UNKNOWN);
