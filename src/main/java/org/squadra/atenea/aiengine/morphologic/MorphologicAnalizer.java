d8eb04fd70a57d67f5cd1a8fa6ae44a20980201f
lea <lea@localhost>
2013-08-23 01:36:27 -0300

    Integraci?n con AteneaParser.

--- src/main/java/org/squadra/atenea/aiengine/morphologic/MorphologicAnalizer.java
+++ src/main/java/org/squadra/atenea/aiengine/morphologic/MorphologicAnalizer.java
@@ -4,9 +4,9 @@ public class MorphologicAnalizer {
 
 	public static String execute(String inputSentence) {
 		
-		//TODO: Aca hay que adaptar la oracion recibida de Google al formato requerido por el CG3
-		
 		String outputSentence = inputSentence;
+		
+		//TODO: Aca hay que adaptar la oracion recibida de Google al formato requerido por el CG3
 		
 		return outputSentence;
 	}
