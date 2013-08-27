6bc9f174c94bd40ddfe5b9ba4abcddec56515c2c
facu <facu@localhost>
2013-08-26 22:12:07 -0300

    Modifico el "algoritmo de respuesta muy simple" para que ejecute acciones con "abrir" o "cerrar"

--- src/main/java/org/squadra/atenea/aiengine/algorithm/VerySimpleResponseAlgorithm.java
+++ src/main/java/org/squadra/atenea/aiengine/algorithm/VerySimpleResponseAlgorithm.java
@@ -2,9 +2,12 @@ package org.squadra.atenea.aiengine.algorithm;
 
 import java.util.ArrayList;
 
+import lombok.extern.log4j.Log4j;
+
 import org.squadra.atenea.ateneacommunication.Message;
 import org.squadra.atenea.data.query.BasicQuery;
 
+@Log4j
 public class VerySimpleResponseAlgorithm  implements AbstractAlgorithm{
 
 	public Message execute(Message inputMessage) {
@@ -13,38 +16,45 @@ public class VerySimpleResponseAlgorithm  implements AbstractAlgorithm{
 		
 		Message response = new Message();
 		
-		BasicQuery query = new BasicQuery();
+		//BasicQuery query = new BasicQuery();
 		
-		ArrayList<String> responses = query.getRelatedWords( inputText );
+		//ArrayList<String> responses = query.getRelatedWords( inputText );
 		
-		if ( responses != null && 
-			 !responses.isEmpty() && 
-			 !responses.get(0).isEmpty() ){
-			
-			if (inputText.contains("abrir") || inputText.contains("cerrar")) {
+//		if ( responses != null && 
+//			 !responses.isEmpty() && 
+//			 !responses.get(0).isEmpty() )
+		{
+//			
+			if (inputText.contains("abrir") || inputText.contains("cerrar")) 
+			{
 				response.setType(Message.ORDER);
-				switch (inputText) {
-					case "abrir bloc de notas":
-						response.setOrder("notepad.exe");
-						break;
-					case "cerrar bloc de notas":
-						response.setOrder("taskkill /IM notepad.exe");
-						break;
-					case "abrir panel de control":
-						response.setOrder("control");
-						break;
-					case "abrir administrador de tareas":
-						response.setOrder("taskmgr");
-						break;
-				}
+				response.setOrder(inputText);
+				System.out.println(inputText);
+				
+//				switch (inputText) {
+//					case "abrir bloc de notas":
+//						response.setOrder("notepad.exe");
+//						break;
+//					case "cerrar bloc de notas":
+//						response.setOrder("taskkill /IM notepad.exe");
+//						break;
+//					case "abrir panel de control":
+//						response.setOrder("control");
+//						break;
+//					case "abrir administrador de tareas":
+//						response.setOrder("taskmgr");
+//						break;
+//				}
+				response.setText("Entendido");
 			}
 			else {
-				response.setType(Message.ASSERTION);
+				//response.setType(Message.ASSERTION);
+				response = new Message("Disculpa, no logre entenderte, por favor repitemelo.", Message.UNKNOWN);
 			}
-			response.setText(responses.get(0));
 			
-		} else {
-			response = new Message("Disculpa, no logre entenderte, por favor repitemelo.", Message.UNKNOWN);
+			
+//		} else {
+//			response = new Message("Disculpa, no logre entenderte, por favor repitemelo.", Message.UNKNOWN);
 		}
 		
 		return response;
