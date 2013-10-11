1def1b3d946d8ee28b489ffe0755daf4be3b76ac
lea <lea@localhost>
2013-10-10 23:36:27 -0300

    INTEGRACION de ordenes al algoritmo de IA. Agrego algoritmo b?sico de reconocimiento de ?rdenes en el sem?ntico.

---
+++ src/main/java/org/squadra/atenea/aiengine/responses/OrderResponseSearcher.java
@@ -0,0 +1,18 @@
+package org.squadra.atenea.aiengine.responses;
+
+public class OrderResponseSearcher {
+
+	/**
+	 * Selecciona de la base de datos una respuesta aleatoria a una orden.
+	 * @param inputMessageType Tipo de mensaje de entrada del usuario.
+	 * @return Respuesta final que se devolverá al usuario.
+	 */
+	public static String getRandomResponse(String inputMessageType) {
+		
+		String finalResponse = "Entendido";
+		
+		// TODO: buscar en la base de datos
+		
+		return finalResponse;
+	}
+}
