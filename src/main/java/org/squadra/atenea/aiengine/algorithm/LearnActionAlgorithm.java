c717b3c55b2d75bce46965460e1862e82fae714e
lucas <lucas@localhost>
2013-10-21 23:48:23 -0300

    Se agregan msgs por pantalla

--- src/main/java/org/squadra/atenea/aiengine/algorithm/LearnActionAlgorithm.java
+++ src/main/java/org/squadra/atenea/aiengine/algorithm/LearnActionAlgorithm.java
@@ -12,14 +12,16 @@ public class LearnActionAlgorithm implements AbstractAlgorithm {
 	public Message execute(Message input) {
 
 		ArrayList <Click> clicks = new ArrayList<Click>();
-		
+		System.out.println("Empiezo a deserializar");
+		System.out.println(input.getIcons().size());
 		for (String icon : input.getIcons()) {
+			System.out.println("recibi: " + icon);
 			clicks.add( Click.deserialize(icon));
 		}
-		
+		System.out.println("Termino de deserializar");
 		ListOfAction.getInstance().addAction(input.getText() , clicks);
 		ListOfAction.getInstance().writeToFile();
-		
+		System.out.println("Termino de hacer todo :'D");
 		return new Message("genial, ahora conozco esta accion");
 	}
 
